8b1d369d893ee52487b7c4eb8c089161a2a03423
Jaredalv7 <jaredalvareze@gmail.com>
2026-02-27 10:49:03 -0700

    Agregando archivo de pruebas

---
+++ test_calculator.py
@@ -0,0 +1,8 @@
+import calculator
+
+assert calculator.sumar(2, 3) == 5
+assert calculator.restar(5, 3) == 2
+assert calculator.multiplicar(4, 2) == 8
+assert calculator.dividir(10, 2) == 5
+
+print("Todas las pruebas pasaron correctamente ✅")
