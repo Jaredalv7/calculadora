3d5cf34a1bedec6bb6655f3f486dbf10e504fb65
Jaredalv7 <jaredalvareze@gmail.com>
2026-03-05 23:19:14 -0700

    forzando fallo en el test

--- test_calculator.py
+++ test_calculator.py
@@ -1,6 +1,6 @@
 import calculator
 
-assert calculator.sumar(2, 3) == 5
+assert calculator.sumar(2, 3) == 8
 assert calculator.restar(5, 3) == 2
 assert calculator.multiplicar(4, 2) == 8
 assert calculator.dividir(10, 2) == 5
